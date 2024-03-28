85378ccdfa2acc3e5e3d8e4eb38811bdfa650ddf
gass <gass@localhost.localdomain>
2024-03-28 03:26:29 +0100

    create account

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+import {countryCodes} from '../../params/country-prefix'
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent  implements OnInit {
-
+  @ViewChild(IonModal) modal!: IonModal;
+  country: any
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.country= countryCodes
+  }
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
 
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+
+  }
 }
